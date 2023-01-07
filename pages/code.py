546121919102d613a355cac22830048fdc01346f
Ahmed OSMAN <78887980+AhmedOsman00py@users.noreply.github.com>
2023-01-07 22:42:11 +0100

    Create code.py

---
+++ pages/code.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+"# Code"
+"### Pour accéder au code"
+"Code de la page simulation-garch : [link](https://github.com/CouLiBaLy-B/GARCH-estimation/blob/main/simulated-garch.py)"
+"Code de la page predictions-with-real-data : [link](https://github.com/CouLiBaLy-B/GARCH-estimation/blob/main/pages/predictions_with_real_data.py)"
