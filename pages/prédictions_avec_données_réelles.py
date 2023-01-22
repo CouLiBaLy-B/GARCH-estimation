609438b8af521589f8a0ad82768e64e668ce5264
COULIBALY Bourahima <91073443+CouLiBaLy-B@users.noreply.github.com>
2023-01-22 17:06:08 +0100

    Update prédictions_avec_données_réelles.py

--- pages/prédictions_avec_données_réelles.py
+++ pages/prédictions_avec_données_réelles.py
@@ -397,10 +397,10 @@ sigma_forecast = prediction()
 sigma_forecast
 
 fig, ax = plt.subplots()
-ax.plot(Data.index[-horizon:], series[-horizon:], 'b-')
+ax.plot(range(horizon, series[-horizon:], 'b-')
 #ax.plot(Data.index[-horizon:], sigma_2[-100:], 'r-')
 #ax.plot([Data.index[-1] + relativedelta(days=i) for i in range(0, horizon)], X_forecast, 'b--')
-ax.plot([Data.index[-1] + relativedelta(days=i) for i in range(0, horizon)], sigma_forecast, 'r--')
+ax.plot(range(horizon), sigma_forecast, 'r--')
 plt.xlabel('Time')
 #plt.legend(['Log-return', 'sigma', "pred-sigma"])
 fig2 = mpl_to_plotly(fig)
