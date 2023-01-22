0dcbab70b5d4d3e29b08d2ff8507078fd048805c
COULIBALY Bourahima <91073443+CouLiBaLy-B@users.noreply.github.com>
2023-01-22 20:21:21 +0100

    Update prédictions_avec_données_réelles.py

--- pages/prédictions_avec_données_réelles.py
+++ pages/prédictions_avec_données_réelles.py
@@ -389,7 +389,7 @@ def sum_beta_omega_theta(L, alpha_estimate ,beta_estimate, omega_estimate, serie
     return result
 
 
-def prediction(series = series, horizon = horizon, L = 10, alpha_estimate = alpha_estimate , beta_estimate = beta_estimate, omega_estimate = omega_estimate):
+def prediction(series = series, horizon = horizon, L = 100, alpha_estimate = alpha_estimate , beta_estimate = beta_estimate, omega_estimate = omega_estimate):
     sigma_forecast =[]
     for i in range(-horizon, 0,1):
         result = sum_beta_omega_theta(L, alpha_estimate , beta_estimate, omega_estimate, series, i-horizon)
@@ -402,12 +402,12 @@ sigma_forecast = prediction(horizon = horizon)
 sigma_forecast
 
 fig, ax = plt.subplots()
-#ax.plot(range(horizon), series[-horizon:], 'b-')
+ax.plot(Data.index[-horizon:], series[-horizon:], 'b-')
 ax.plot(Data.index[-horizon:], sigma_2[-horizon:], 'r-')
 #ax.plot([Data.index[-1] + relativedelta(days=i) for i in range(0, horizon)], X_forecast, 'b--')
-ax.plot(Data.index[-horizon:], sigma_forecast, 'b-')
-#plt.xlabel('Time')
-#plt.legend(['Log-return', 'sigma', "pred-sigma"])
+ax.plot(Data.index[-horizon:], sigma_forecast, 'b--')
+plt.xlabel('Time')
+plt.legend(['Log-return', 'sigma', "pred-sigma"])
 fig2 = mpl_to_plotly(fig)
 fig2
 plt.show()
